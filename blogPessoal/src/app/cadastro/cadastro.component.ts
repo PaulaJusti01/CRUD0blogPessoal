0d18ccf1dcc3f44fa77cde1b6bdcb3695c214bea
Paula Justi da Silva <paulajusti01@hotmail.com>
2020-10-02 15:25:42 -0300

    Consumo de api

---
+++ blogPessoal/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,43 @@
+import { AlertasService} from './../service/alertas.service';
+import { AuthService } from './../service/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  user: User = new User()
+  senha: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alerta: AlertasService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  conferirSenha(event: any) {
+    this.senha = event.target.value
+  }
+
+  cadastrar() {
+    if( this.senha === this.user.senha){
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+    })
+    }else{
+      alert('Sua senhas não conferem ')
+    }
+    
+  }
+}
